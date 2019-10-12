047df713d4c7aeacc810a3fbfc7139b99a1fe11c
Semion Nadezhdin <me@sg-studio.ru>
2019-10-13 01:42:07 +0300

    Added Static Prefab example(ugly)

---
+++ ExampleMod/Runtime/ExampleMod/GoldenChickenMagnument.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using Ananke.Attachment.Core.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using SignsOfLife;
+using SignsOfLife.Prefabs.StaticPrefabs;
+using SignsOfLife.Utils;
+
+namespace Runtime.ExampleMod
+{
+    public class GoldenChickenMagnument : StaticPrefab
+    {
+        private static readonly int[,] TileData =
+        {
+            {0, 0},
+            {0, 0}
+        };
+
+        public GoldenChickenMagnument(
+            StaticPrefabType staticPrefabType,
+            DumbGraphicsRegistry.ResourceDefinition textureResource)
+            : base("Golden Chicken Magnument", TileData, staticPrefabType)
+        {
+            _cutOutBounds = true;
+            Description = "Indicates your domain.";
+            _texture = textureResource.GetRenderable().Item1;
+            _currentBounds = textureResource.GetRenderable().Item2;
+        }
+
+        private Texture2D _texture;
+        private Rectangle _currentBounds;
+
+        //TODO: I believe this is INWORLD drawing
+        public override void Draw(
+            SpriteBatch spriteBatch,
+            Vector2 screenGameFieldPosition,
+            Matrix transformMatrix)
+        {
+            spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend, null,
+                null, _scissorRasterizerState, null,
+                transformMatrix);
+            Rectangle scissorRectangle = spriteBatch.GraphicsDevice.ScissorRectangle;
+            if (_cutOutBounds)
+                HandleScissorRect(spriteBatch, screenGameFieldPosition);
+            spriteBatch.Draw(_texture,
+                new Vector2(Position.X, Position.Y) -
+                screenGameFieldPosition, _currentBounds, Color.White);
+            if (_cutOutBounds)
+                spriteBatch.GraphicsDevice.ScissorRectangle = scissorRectangle;
+            spriteBatch.End();
+        }
+
+        //TODO: I believe this is a PREVIEW drawing
+        public override void Draw(
+            SpriteBatch spriteBatch,
+            float screenX,
+            float screenY,
+            Color color,
+            Matrix transformMatrix)
+        {
+            spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, null,
+                null, null, null, transformMatrix);
+            spriteBatch.Draw(_texture, new Vector2(screenX, screenY),
+                _currentBounds, Color.White);
+            spriteBatch.End();
+        }
+
+        //TODO: WUT?
+        public override bool IsAtPointPreciseSlow(Vector2 globalPoint)
+        {
+            if (!IsAtPoint(globalPoint))
+                return false;
+            if (globalPoint.X < 0.0)
+                globalPoint.X = Map.GetWorldBounds().Width + globalPoint.X;
+            if (globalPoint.X > (double) Map.GetWorldBounds().Width)
+                globalPoint.X -= Map.GetWorldBounds().Width;
+            if (globalPoint.X > (double) X &&
+                globalPoint.X < X + (double) Width &&
+                (globalPoint.Y > (double) Y &&
+                 globalPoint.Y < Y + (double) Height))
+            {
+                double num1 = globalPoint.X - (double) Map._screenGameFieldPosition.X;
+                float num2 = globalPoint.Y - Map._screenGameFieldPosition.Y;
+                float num3 = X + _animationOffset.X - Map._screenGameFieldPosition.X;
+                float num4 = Y + _animationOffset.Y - Map._screenGameFieldPosition.Y;
+                double num5 = num3;
+                int num6 = (int) Math.Floor(num1 - num5);
+                int num7 = (int) Math.Floor(num2 - (double) num4);
+                if (Flipped)
+                    num6 = Width - num6;
+                try
+                {
+                    if (!TextureUtils.IsPixelOnTextureTransparent(_texture, _currentBounds.X + num6,
+                        _currentBounds.Y + num7))
+                        return true;
+                }
+                catch (Exception ex)
+                {
+                    Logger.Echo(ex.ToString());
+                    Logger.WriteLine(ex.ToString());
+                    return false;
+                }
+            }
+
+            return false;
+        }
+    }
+}
--- ExampleMod/Runtime/Mod.cs
+++ ExampleMod/Runtime/Mod.cs
@@ -5,11 +5,13 @@ using Ananke.Attachment.Core.Graphics;
 using Ananke.Attachment.Core.Items;
 using Ananke.Attachment.Core.Mod;
 using Ananke.Attachment.Core.Phases;
+using Ananke.Attachment.Core.StaticPrefabs;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Runtime.ExampleMod;
 using SignsOfLife.Crafting;
 using SignsOfLife.Entities.Items;
+using SignsOfLife.Prefabs.StaticPrefabs;
 
 namespace Runtime
 {
@@ -21,6 +23,10 @@ namespace Runtime
             var spriteGoldenChicken = AnankeContext.Current.DumbGraphicsRegistry.DefineResource(
                 new FileInfo(@"C:\MODDING\Signs of Life\mods\example\assets\goldenchicken.png"),
                 new Rectangle(17, 10, 32, 44));
+            
+            var spriteGoldenChickenMagnument = AnankeContext.Current.DumbGraphicsRegistry.DefineResource(
+                new FileInfo(@"C:\MODDING\Signs of Life\mods\example\assets\goldenchicken_magnument.png"),
+                new Rectangle(0, 0, 64, 64));            
 
             var itemDefinitionGoldenChicken = new ItemDefinition(
                 50001,
@@ -28,6 +34,19 @@ namespace Runtime
                 new ItemActivator(() => new GoldenChickenItem(50001, spriteGoldenChicken)));
             context.ItemsRegistry.Add(itemDefinitionGoldenChicken);
 
+            //TODO: MY GOD!!! IT IS SO UGLY!
+            StaticPrefabType sptMagnument = (StaticPrefabType) 2000;
+            var staticPrefabDefintionMagnument = new StaticPrefabDefinition(
+                (long) sptMagnument,
+                "golden_chincken_magnument",
+                new StaticPrefabActivator(
+                    (() => new GoldenChickenMagnument(sptMagnument,
+                        spriteGoldenChickenMagnument)
+                    )
+                )
+            );
+            context.StaticPrefabRegistry.Add(staticPrefabDefintionMagnument);
+            
             context.Compatibility.V1Compat.AddActionForRecipeLoadingPhase(
                 GetType(), ctx =>
                 {
@@ -42,9 +61,24 @@ namespace Runtime
                         HideUnlessDebug = false,
                         RequiredMaterials = new[]
                         {
-                            new RequiredRecipeMaterial(InventoryItemType.GOLD_INGOT, 50),
+                            new RequiredRecipeMaterial(InventoryItemType.GOLD_INGOT, 1),
                         }.ToList(),
                         ResultItem = new ResultRecipeItem((InventoryItemType) itemDefinitionGoldenChicken.Id, 1)
+                    });
+                    //TODO: Too ugly
+                    SignsOfLife.UI.Hud._allRecipes.Add(new Recipe
+                    {
+                        Name = "Golden Chicken Magnument",
+                        AutoGrant = true,
+                        Category = "Swag",
+                        CraftingCategory = "Projects",
+                        HideUnlessDebug = false,
+                        RequiredMaterials = new[]
+                        {
+                            new RequiredRecipeMaterial(InventoryItemType.GOLD_INGOT, 5),
+                        }.ToList(),
+                        ResultItem = new ResultRecipeItem(InventoryItemType.NULL, 1)
+                            {StaticPrefabType = (StaticPrefabType) staticPrefabDefintionMagnument.Id}
                     });
                 }
             );
