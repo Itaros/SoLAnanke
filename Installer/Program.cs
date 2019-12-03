edea3e1a7462e0ba8ae75fc81fd41996088c44e4
Semion Nadezhdin <me@sg-studio.ru>
2019-12-03 23:40:46 +0300

    Improved installation process

--- Installer/Program.cs
+++ Installer/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using Installer.Instrumentation;
 using Mono.Cecil;
 
@@ -9,6 +10,12 @@ namespace Installer
     {
         public static void Main(string[] args)
         {
+
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Running in dry mode.");
+                Console.WriteLine("You can link with Ananke core with: -ananke");
+            }
             
             AnankeContext ananke = new AnankeContext();
             
@@ -26,10 +33,18 @@ namespace Installer
                 new ReplaceVersion(),
                 new FixupHackySpecialCases(), 
                 new AddAttachmentAnankeCore(),
-                new ActivateAttachmentAnankeCore(), 
-                new RegisterAnankeCoreOverrides(),
                 new UnlockAccess(),
             };
+
+            if (args.Length>=1 && args[0] == "-ananke")
+            {
+                instrumentations = instrumentations.Concat(new AbstractInstrumentation[]
+                {
+                    new ActivateAttachmentAnankeCore(), 
+                    new RegisterAnankeCoreOverrides()
+                }).ToArray();
+            }
+            
             foreach (var instrumentation in instrumentations)
             {
                 Console.WriteLine($"Applying instrumentation: {instrumentation.Name}...");
